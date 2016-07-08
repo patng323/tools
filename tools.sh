432b85f45b60e1398a402775bc20f6369741c67e
Patrick Ng <patrick.ng@networld.hk>
2016-07-08 11:59:49 +0800

    awk and total

--- tools.sh
+++ tools.sh
@@ -2,7 +2,8 @@
 alias catlast="ls -ltr | tr -s ' ' | cut -d ' ' -f 9 | tail -n 1 | xargs cat"
 alias lesslast="ls -ltr | tr -s ' ' | cut -d ' ' -f 9 | tail -n 1 | xargs less"
 
-###########################
+
+############
 # How to cut a file, i.e. select a portion of a file by fields or char columns
 
 # Display fields 1 and 7 of the file /etc/passwd, with delimiter = ':'
@@ -20,3 +21,12 @@ who | cut -c 1-16,26-38
 # Treat continuous spaces as a single delimiter
 cat text.txt | tr -s ' ' | cut -d ' ' -f 4
 
+
+############
+# Sum up some fields
+
+# Total of 2nd field
+awk -F"," '{x+=$2}END{print x}' file
+
+# Sub-total of 2nd field where 1st column == 'Item1'
+awk -F, '$1=="Item1"{x+=$2;}END{print x}' file
